fc7bc51fc966dd427ee1049219fa345b958df415
rosasherly <rosasherly979@gmail.com>
2026-08-02 15:01:33 +0700

    Add Form Request validation

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'price' => 'required|numeric',
+            'description' => 'nullable|string',
+            'stock' => 'required|integer|min:0',
+        ];
+    }
+}
