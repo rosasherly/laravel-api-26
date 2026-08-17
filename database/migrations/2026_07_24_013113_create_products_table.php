e741c00047724988d3b504b64b2b370f7c170fc4
rosasherly <rosasherly979@gmail.com>
2026-08-17 23:27:54 +0700

    fix: update products migration

--- database/migrations/2026_07_24_013113_create_products_table.php
+++ database/migrations/2026_07_24_013113_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,9 +14,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name',100);
-            $table->decimal('price', 10,0);
+            $table->string('name', 100);
+            $table->decimal('price', 10, 0);
             $table->text('description');
+            $table->foreignIdFor(Category::class, 'category_id');
             $table->integer('stock');
             $table->timestamps();
         });
