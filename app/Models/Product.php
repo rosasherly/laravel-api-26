8c59ea4f01dbf72b82e11e3825d904d024b2a6c7
rosasherly <rosasherly979@gmail.com>
2026-08-17 23:27:27 +0700

    fix: update product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'description', 'stock'];
+    protected $fillable = ['name', 'price', 'description', 'stock', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
