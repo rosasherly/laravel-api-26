3c2cfd40c8fe7282087101e1c842a5f913cdbf69
rosasherly <rosasherly979@gmail.com>
2026-09-15 07:32:04 +0700

    feat(user): add UserController for user management

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function profile()
+    {
+        return 'success';
+        
+    }
+}
